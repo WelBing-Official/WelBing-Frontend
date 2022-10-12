852fe6d745db10ce7f290046edfd52f4984115ab
Praisecodes <cmancoder@gmail.com>
2022-10-12 21:45:30 +0100

    Messing around, no vex :-)

--- src/pages/waitlist.jsx
+++ src/pages/waitlist.jsx
@@ -1,5 +1,9 @@
 export default function WaitList(){
     return (
-        <></>
+        <>
+        <div className="p-4 text-white bg-black pic text-2xl h-44">
+            Hello World
+        </div>
+        </>
     )
 }
