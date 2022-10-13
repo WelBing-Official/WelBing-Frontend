3451433763332b4bf18e50d159fcd721da098295
Praisecodes <cmancoder@gmail.com>
2022-10-14 00:05:30 +0100

    Done with form design

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -4,20 +4,33 @@ export default function Modal(){
         <input type="checkbox" id="my-modal" className="modal-toggle" />
         <div className="modal">
             <div className="modal-box bg-white rounded-md flex flex-col items-center lg:w-hIw w-11/12 max-w-5xl px-1">
-                <h1 className=" font-nunito font-bold text-xl">JOIN WAITLIST</h1><br />
+                <h1 className=" font-nunito font-bold text-xl">JOIN THE WAITLIST</h1><br />
 
-                <form onSubmit={(e)=>{e.preventDefault()}} className="w-4/5">
+                <form onSubmit={(e)=>{e.preventDefault()}} className="lg:w-4/5 w-11/12 flex flex-col justify-center items-center">
                     <input 
                     type="text" 
                     className="input focus:outline-none text-gray-700 font-nunito bg-white border-solid border-gray-400 rounded-sm w-full" 
                     placeholder="Enter Your Name" 
-                    /> <br /> <br />
+                    /> <br />
                     <input 
                     type="email" 
                     className="input focus:outline-none text-gray-700 font-nunito bg-white border-solid border-gray-400 rounded-sm w-full" 
                     placeholder="Enter Your Email" 
-                    /> <br /><br />
-                    <label htmlFor="my-modal" className="btn bg-mainBlue hover:bg-mainBlue rounded px-20 font-lato font-bold text-white border-0">
+                    /> <br />
+                    <h2 className="w-full font-nunito text-black text-lg">Select A Category</h2>
+                    <select className="input mt-2 focus:outline-none text-md text-gray-700 border-gray-400 rounded-sm w-full font-nunito font-bold">
+                        <option value={null}>- Choose Your Category -</option>
+                        <option value="Public Sector Doctor">
+                            Public Sector Doctor
+                        </option>
+                        <option value="Private Sector Doctor">
+                            Private Sector Doctor
+                        </option>
+                        <option value="Individual (Non-Health Professional)">
+                            Individual (Non-Health Professional)
+                        </option>
+                    </select> <br />
+                    <label htmlFor="my-modal" className="btn bg-mainBlue hover:bg-mainBlue rounded w-4/5 font-lato font-bold text-white border-0">
                         Join Now
                     </label>
                 </form>
