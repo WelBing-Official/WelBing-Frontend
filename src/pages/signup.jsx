7d0cb4c4242d8a7ba5a0fd2ea8644796b7328dc8
Praisecodes <cmancoder@gmail.com>
2022-10-16 02:20:52 +0100

    adding bg

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -9,7 +9,7 @@ export default function Signup(){
             <label className="font-lato text-sm text-gray-500 mr-2">
                 Account Type
             </label> <br />
-            <select className="input font-nunito focus:outline-none rounded-sm">
+            <select className="input bg-white font-nunito focus:outline-none rounded-sm">
                 <option value="Patients Account">Patients Account</option>
                 <option value="Hospital/Government Bodies Account">Hospital/Government Bodies Account</option>
                 <option value="Health Practitioner">Health Practitioner</option>
@@ -19,21 +19,21 @@ export default function Signup(){
             </label> <br />
             <input 
             type="text"
-            className="input mb-3 focus:outline-none shadow-md rounded-sm text-sm text-gray-800 font-nunito" 
+            className="input bg-white mb-3 focus:outline-none shadow-md rounded-sm text-sm text-gray-800 font-nunito" 
             /> <br />
             <label className="font-lato text-sm text-gray-500 mr-2">
                 Email
             </label> <br />
             <input 
             type="email"
-            className="input mb-3 focus:outline-none shadow-md rounded-sm text-sm text-gray-800 font-nunito" 
+            className="input bg-white mb-3 focus:outline-none shadow-md rounded-sm text-sm text-gray-800 font-nunito" 
             /> <br />
             <label className="font-lato text-sm text-gray-500 mr-2">
                 Password
             </label> <br />
             <input 
             type="password"
-            className="input mb-3 focus:outline-none shadow-md rounded-sm text-sm text-gray-800 font-nunito" 
+            className="input bg-white mb-3 focus:outline-none shadow-md rounded-sm text-sm text-gray-800 font-nunito" 
             /> <br />
             <button type="submit" className="btn no-animation bg-secondColor text-white w-4/5 m-auto border-0 hover:bg-secondColor">
                 Register
