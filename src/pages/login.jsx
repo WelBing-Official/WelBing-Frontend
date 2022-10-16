a72690c78ef2d2ce77f6c1b97e6ab170372270ce
Praisecodes <cmancoder@gmail.com>
2022-10-16 09:21:31 +0100

    Login feature

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,18 +1,82 @@
 import AuthLayout from "../layouts/authlayout"
 import { Link, useNavigate } from "react-router-dom"
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"
 
 export default function Login(){
+    const categories = {
+        "Patients Account": "basic",
+        "Hospital/Government Bodies Account": "hospital",
+        "Health Practitioner": "practitioner"
+    };
+
+    const [loading, setLoading] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [acctype, setAcctype] = useState("Patients Account");
+
+    const loginUser = () =>{
+        setLoading(!loading)
+        if(email == "" || password == ""){
+            toast.error("Fill in all fields", {
+                draggable: false,
+                position: toast.POSITION.TOP_RIGHT
+            });
+            setLoading(false);
+        }
+        else{
+            fetch("https://welbing-waitlist.herokuapp.com/login", {
+                method: 'POST',
+                body: JSON.stringify({
+                    'email': email,
+                    "password": password,
+                    'category': categories[`${acctype}`]
+                })
+            })
+            .then(res=>res.json())
+            .then((data)=>{
+                let {resolved, message} = data;
+
+                switch(message){
+                    case 'Incorrect email or password':
+                        toast.error("Incorrect Details", {
+                            draggable: false,
+                            position: toast.POSITION.TOP_RIGHT
+                        });
+                        break;
+                    default:
+                        toast.success("Login Successful", {
+                            draggable: false,
+                            position: toast.POSITION.TOP_RIGHT
+                        })
+                }
+                setLoading(false);
+            })
+            .catch((err)=>{
+                console.log(err);
+                setLoading(false);
+            })
+        }
+    }
+
     let nav = useNavigate();
     return (
         <AuthLayout page="login">
+            <ToastContainer />
             <h1 className="text-mainBlue font-nunito text-3xl font-bold">
                 Login
             </h1> <br /> <br />
+            <select className="input bg-white font-nunito shadow-md focus:outline-none rounded-sm" onChange={(e)=>{setAcctype(e.target.value)}}>
+                <option value="Patients Account">Patients Account</option>
+                <option value="Hospital/Government Bodies Account">Hospital/Government Bodies Account</option>
+                <option value="Health Practitioner">Health Practitioner</option>
+            </select> <br />
             <label className="text-gray-500 font-lato mr-2 ">Email</label> <br />
-            <input type="text" className="input font-nunito text-sm bg-white text-gray-800 focus:outline-none" /> <br /> <br />
+            <input type="text" className="input font-nunito text-sm bg-white text-gray-800 focus:outline-none" onChange={(e)=>{setEmail(e.target.value)}} /> <br /> <br />
 
             <label className="text-gray-500 font-lato mr-2 ">Password</label> <br />
-            <input type="password" className="input font-nunito text-sm bg-white text-gray-800 focus:outline-none" /> <br />
+            <input type="password" className="input font-nunito text-sm bg-white text-gray-800 focus:outline-none" onChange={(e)=>{setPassword(e.target.value)}} /> <br />
 
             <div className="flex justify-between items-center p-2 w-4/5 m-auto">
                 <div className="flex items-center">
@@ -28,7 +92,7 @@ export default function Login(){
             <button 
             type="submit" 
             className="bg-secondColor text-white font-nunito btn w-4/5 m-auto border-0 no-animation hover:bg-secondColor"
-            onClick={()=>{nav("/account")}}
+            onClick={()=>{loginUser()}}
             >
                 Login Now
             </button>
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,15 +1,89 @@
 import AuthLayout from "../layouts/authlayout"
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Signup(){
+    const categories = {
+        "Patients Account": "basic",
+        "Hospital/Government Bodies Account": "hospital",
+        "Health Practitioner": "practitioner"
+    };
+
+    const [loading, setLoading] = useState(false);
+    const [typeacc, setType] = useState("Patients Account");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signupUser = () =>{
+        setLoading(!loading)
+        if(name == "" || email == "" || password == ""){
+            toast.error("Please Fill All Fields", {
+                draggable: false,
+                position: toast.POSITION.TOP_RIGHT
+            })
+            setLoading(false)
+        }
+        else{
+            console.log(typeacc)
+            fetch("https://welbing-waitlist.herokuapp.com/register", {
+                method: 'POST',
+                body: JSON.stringify({
+                    'name': name,
+                    'email': email,
+                    'password': password,
+                    'category': categories[`${typeacc}`]
+                })
+            })
+            .then(res=>res.json())
+            .then((data)=>{
+                console.log(data)
+                let {resolved, message, addon} = data;
+                switch(message){
+                    case 'Input error':
+                        toast.error("Internal Server Error!!", {
+                            draggable: false,
+                            position: toast.POSITION.TOP_RIGHT
+                        });
+                        break;
+                    case 'exists':
+                        toast.error("This user already exists", {
+                            draggable: false,
+                            position: toast.POSITION.TOP_RIGHT
+                        });
+                        break;
+                    case 'Server Error':
+                        toast.error("Fatal Error!", {
+                            draggable: false,
+                            position: toast.POSITION.TOP_RIGHT
+                        });
+                        break;
+                    default:
+                        toast.success("Registered Successfully!", {
+                            draggable: false,
+                            position: toast.POSITION.TOP_RIGHT
+                        })
+                }
+                setLoading(false)
+            })
+            .catch((err)=>{
+                setLoading(false)
+                console.log(err)
+            })
+        }
+    }
+
     return (
         <AuthLayout page="signup">
+            <ToastContainer />
             <h1 className="font-nunito text-3xl text-mainBlue font-bold">
                 Register
             </h1> <br />
             <label className="font-lato text-sm text-gray-500 mr-2">
                 Account Type
             </label> <br />
-            <select className="input bg-white font-nunito focus:outline-none rounded-sm">
+            <select className="input bg-white font-nunito shadow-md focus:outline-none rounded-sm" onChange={(e)=>{setType(e.target.value)}}>
                 <option value="Patients Account">Patients Account</option>
                 <option value="Hospital/Government Bodies Account">Hospital/Government Bodies Account</option>
                 <option value="Health Practitioner">Health Practitioner</option>
@@ -20,6 +94,7 @@ export default function Signup(){
             <input 
             type="text"
             className="input bg-white mb-3 focus:outline-none shadow-md rounded-sm text-sm text-gray-800 font-nunito" 
+            onChange={(e)=>{setName(e.target.value)}}
             /> <br />
             <label className="font-lato text-sm text-gray-500 mr-2">
                 Email
@@ -27,6 +102,7 @@ export default function Signup(){
             <input 
             type="email"
             className="input bg-white mb-3 focus:outline-none shadow-md rounded-sm text-sm text-gray-800 font-nunito" 
+            onChange={(e)=>{setEmail(e.target.value)}}
             /> <br />
             <label className="font-lato text-sm text-gray-500 mr-2">
                 Password
@@ -34,9 +110,10 @@ export default function Signup(){
             <input 
             type="password"
             className="input bg-white mb-3 focus:outline-none shadow-md rounded-sm text-sm text-gray-800 font-nunito" 
+            onChange={(e)=>{setPassword(e.target.value)}}
             /> <br />
-            <button type="submit" className="btn no-animation bg-secondColor text-white w-4/5 m-auto border-0 hover:bg-secondColor">
-                Register
+            <button type="submit" className="btn no-animation bg-secondColor text-white w-4/5 m-auto border-0 hover:bg-secondColor" onClick={()=>{signupUser()}}>
+                {(loading)?"Please Wait" : "Register"}
             </button>
         </AuthLayout>
     )
